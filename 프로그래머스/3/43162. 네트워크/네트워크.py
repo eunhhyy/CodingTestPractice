3c6a0347850fea21cc39f23305e03c6277fe4110
eunhhyy <71955621+eunhhyy@users.noreply.github.com>
2024-10-05 20:46:02 +0900

    [level 3] Title: 네트워크, Time: 0.62 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,16 +1,14 @@
 def solution(n, computers):
     def dfs(node):
         visited[node] = True
-        for neighbor in range(n):
-            if computers[node][neighbor] == 1 and not visited[neighbor]:
-                dfs(neighbor)
-    
+        for k in range(n):
+            if computers[node][k] == 1 and visited[k] == False:
+                dfs(k)
+                
     visited = [False] * n
-    network_count = 0
-    
+    cnt = 0
     for i in range(n):
         if not visited[i]:
             dfs(i)
-            network_count += 1
-    
-    return network_count
+            cnt+=1
+    return cnt
